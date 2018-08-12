4d8b5613400325f4c98c43b8712bdc279140d319
arena1974 <arena1974@naver.com>
2018-08-13 00:29:35 +0900

    sample 1

---
+++ NeoSamples/console_sum/Neo1.00.cpp
@@ -0,0 +1,82 @@
+// Neo1.00.cpp: 콘솔 응용 프로그램의 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "Neo1.00.h"
+#include "../../NeoSource/NeoVM.h"
+#include <iostream>
+
+float Mul(float a, float b)
+{
+	return a * b;
+}
+
+void print(const char* p)
+{
+	std::cout << p;
+}
+
+
+
+BOOL        FileLoad(const char* pFileName, void*& pBuffer, int& iLen)
+{
+	FILE* fp = NULL;
+	int error_t = fopen_s(&fp, pFileName, "rb");
+	if (error_t != 0)
+		return FALSE;
+
+	fseek(fp, 0, SEEK_END);
+	int iFileSize = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	pBuffer = new BYTE[iFileSize + 2];
+	fread(pBuffer, iFileSize, 1, fp);
+	fclose(fp);
+
+	iLen = iFileSize;
+	return TRUE;
+}
+
+int main()
+{
+	void* pFileBuffer = NULL;
+	int iFileLen = 0;
+	if (FALSE == FileLoad("1.neo", pFileBuffer, iFileLen))
+	{
+		printf("file read error");
+		return -1;
+	}
+
+
+	int iLenTemp = 1 * 1024 * 1024;
+	BYTE* pCodeTemp = new BYTE[iLenTemp];
+
+	int iCodeLen = 0;
+	if (CNeoVM::Compile(pFileBuffer, iFileLen, pCodeTemp, iLenTemp, &iCodeLen) == TRUE)
+	{
+		printf("Comile Success !!\n");
+		CNeoVM* pVM = CNeoVM::LoadVM(pCodeTemp, iCodeLen);
+		if (NULL != pVM)
+		{
+			pVM->Register("Mul", Mul);
+			pVM->Register("print", print);
+
+			int i = 5;
+			for (int i = 1; i < 10; i++)
+			{
+				DWORD t1 = GetTickCount();
+				float r = pVM->Call<float>("Sum", 100, i);
+				DWORD t2 = GetTickCount();
+
+				printf("\nSum %d + %d = %f (T:%d)", 100, i, r, t2 - t1);
+			}
+			Sleep(10);
+			CNeoVM::ReleaseVM(pVM);
+		}
+	}
+	delete[] pFileBuffer;
+	delete[] pCodeTemp;
+
+    return 0;
+}
+
