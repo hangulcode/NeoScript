cf9f116beff4092d3bca0c3247d5a4faeba522de
arena1974 <arena1974@naver.com>
2018-08-14 23:31:05 +0900

    refactoring

---
+++ NeoSource/NeoConfig.h
@@ -0,0 +1,18 @@
+#pragma once
+
+typedef unsigned char	u8;
+typedef char			s8;
+typedef unsigned short	u16;
+typedef short			s16;
+typedef unsigned int	u32;
+typedef int				s32;
+
+
+#define FILE_NEOS	(('N' << 24) | ('E' << 16) | ('O' << 8) | ('S'))
+#define NEO_VER		(('0' << 24) | ('1' << 16) | ('0' << 8) | ('0'))
+
+#include <vector>
+#include <list>
+#include <string>
+#include <map>
+#include <set>
