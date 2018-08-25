cce1b613ccb76acae9a4f58f7cbec909ce7e6982
arena1974 <arena1974@naver.com>
2018-08-25 12:52:49 +0900

    9x9 단 sample

---
+++ NeoSamples/console_9_times/console_9_times.cpp
@@ -0,0 +1,59 @@
+#include "stdafx.h"
+#include "console_9_times.h"
+#include "../../NeoSource/NeoVM.h"
+
+
+
+
+BOOL        FileLoad(const char* pFileName, void*& pBuffer, int& iLen)
+{
+	FILE* fp = NULL;
+	int error_t = fopen_s(&fp, pFileName, "rb");
+	if (error_t != 0)
+		return false;
+
+	fseek(fp, 0, SEEK_END);
+	int iFileSize = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	pBuffer = new BYTE[iFileSize + 2];
+	fread(pBuffer, iFileSize, 1, fp);
+	fclose(fp);
+
+	iLen = iFileSize;
+	return true;
+}
+
+int main()
+{
+	void* pFileBuffer = NULL;
+	int iFileLen = 0;
+	if (false == FileLoad("1.neo", pFileBuffer, iFileLen))
+	{
+		printf("file read error");
+		return -1;
+	}
+
+	CNeoVM* pVM = CNeoVM::CompileAndLoadVM(pFileBuffer, iFileLen, true);
+	if (pVM != NULL)
+	{
+		for (int i = 1; i < 10; i++)
+		{
+			DWORD t1 = GetTickCount();
+			pVM->Call<void>("Time9", i);
+			DWORD t2 = GetTickCount();
+			if (pVM->IsLastErrorMsg())
+			{
+				printf("Error - VM Call : %s (Elapse:%d)\n", pVM->GetLastErrorMsg(), t2 - t1);
+				pVM->ClearLastErrorMsg();
+			}
+			else
+				printf("(Elapse:%d)\n", t2 - t1);
+		}
+		CNeoVM::ReleaseVM(pVM);
+	}
+	delete[] pFileBuffer;
+
+    return 0;
+}
+
