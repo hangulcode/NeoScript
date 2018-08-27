b4d834f6090f4e1a804505dc31c987a2eae89a7b
arena1974 <arena1974@naver.com>
2018-08-28 00:24:57 +0900

    table callback sample

---
+++ NeoSamples/console_table_callback/console_table_callback.cpp
@@ -0,0 +1,122 @@
+#include "stdafx.h"
+#include "console_table_callback.h"
+#include "../../NeoSource/NeoVM.h"
+
+class CA
+{
+public:
+	bool FunSum(CNeoVMWorker* pN, short args)
+	{
+		if (args != 2)
+			return false;
+
+		double v1 = pN->GetArg_Double(1);
+		double v2 = pN->GetArg_Double(2);
+
+		pN->ReturnValue(v1 + v2);
+
+		return true;
+	}
+	bool FunMul(CNeoVMWorker* pN, short args)
+	{
+		if (args != 2)
+			return false;
+
+		double v1 = pN->GetArg_Double(1);
+		double v2 = pN->GetArg_Double(2);
+
+		pN->ReturnValue(v1 * v2);
+
+		return true;
+	}
+};
+
+bool FunSum(CNeoVMWorker* pN, short args)
+{
+	return ((CA*)pN->GetCallTableInfo()->_pUserData)->FunSum(pN, args);
+}
+bool FunMul(CNeoVMWorker* pN, short args)
+{
+	return ((CA*)pN->GetCallTableInfo()->_pUserData)->FunMul(pN, args);
+}
+
+
+static SFunLib g_sTableFun[] =
+{
+	{ "sum", CNeoVM::RegisterNative(FunSum) },
+	{ "mul", CNeoVM::RegisterNative(FunMul) },
+	{ NULL, FunctionPtrNative() },
+};
+
+
+
+BOOL        FileLoad(const char* pFileName, void*& pBuffer, int& iLen)
+{
+	FILE* fp = NULL;
+	int error_t = fopen_s(&fp, pFileName, "rb");
+	if (error_t != 0)
+		return false;
+
+	fseek(fp, 0, SEEK_END);
+	int iFileSize = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	pBuffer = new BYTE[iFileSize + 2];
+	fread(pBuffer, iFileSize, 1, fp);
+	fclose(fp);
+
+	iLen = iFileSize;
+	return true;
+}
+
+int main()
+{
+	void* pFileBuffer = NULL;
+	int iFileLen = 0;
+	if (false == FileLoad("1.neo", pFileBuffer, iFileLen))
+	{
+		printf("file read error");
+		return -1;
+	}
+
+	CNeoVM* pVM = CNeoVM::CompileAndLoadVM(pFileBuffer, iFileLen, true);
+	if (pVM != NULL)
+	{
+		CA* pClass = new CA();
+
+		VarInfo* g_sData = pVM->Call<VarInfo*>("GetTable");
+		if (g_sData->GetType() == VAR_TABLE)
+		{
+			TableInfo* pTable = g_sData->_tbl;
+			pTable->_pUserData = pClass; // <-------------------
+
+			VarInfo fun;
+			fun.SetType(VAR_TABLEFUN);
+			SFunLib* pFuns = g_sTableFun;
+			while (pFuns->pName != NULL)
+			{
+				fun._fun = pFuns->fn;
+				pTable->_strMap[pFuns->pName] = fun;
+				pFuns++;
+			}
+		}
+
+		DWORD t1 = GetTickCount();
+		double r = pVM->Call<double>("update", 5, 5);
+		DWORD t2 = GetTickCount();
+
+		if (pVM->IsLastErrorMsg())
+		{
+			printf("Error - VM Call : %s (Elapse:%d)\n", pVM->GetLastErrorMsg(), t2 - t1);
+			pVM->ClearLastErrorMsg();
+		}
+		else
+			printf("%lf (Elapse:%d)\n", r, t2 - t1);
+
+		CNeoVM::ReleaseVM(pVM);
+	}
+	delete[] pFileBuffer;
+
+    return 0;
+}
+
