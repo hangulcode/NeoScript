f1e1e6928321d2cb5edab6a23e7038e0366e260a
hangulcode <hangulcode@gmail.com>
2020-05-25 09:57:08 +0900

    ...

--- NeoSamples/console/console.cpp
+++ NeoSamples/console/console.cpp
@@ -57,6 +57,7 @@ int main()
 		printf("6.time_limit\n");
 		printf("7.divide_by_zero\n");
 		printf("8.delegate\n");
+		printf("9.meta\n");
 
 		printf("\npress a key ...\n");
 
@@ -68,37 +69,34 @@ int main()
 			break;
 		case '1':
 			SAMPLE_callback();
-			system("pause");
 			break;
 		case '2':
 			SAMPLE_table_callback();
-			system("pause");
 			break;
 		case '3':
 			SAMPLE_9_times();
-			system("pause");
 			break;
 		case '4':
 			SAMPLE_table();
-			system("pause");
 			break;
 		case '5':
 			SAMPLE_slice_run();
-			system("pause");
 			break;
 		case '6':
 			SAMPLE_time_limit();
-			system("pause");
 			break;
 		case '7':
 			SAMPLE_etc("etc.neo", "divide_by_zero");
-			system("pause");
 			break;
 		case '8':
 			SAMPLE_etc("delegate.neo", "delegate");
-			system("pause");
+			break;
+		case '9':
+			SAMPLE_etc("meta.neo", "meta");
 			break;
 		}
+		if(blEnd == false)
+			system("pause");
 	}
 
     return 0;
